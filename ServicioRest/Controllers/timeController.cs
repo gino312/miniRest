0eaa7bf2074cf9ad07bb568ed5a81712f041ab9e
Gino Cuevas <gino.cuevasw@gmail.com>
2017-01-14 04:37:37 -0300

    Subida miniRest
    Se subiran todos los archivos del proyecto al repositorio

---
+++ ServicioRest/Controllers/timeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ServicioRest.Controllers
+{
+    public class timeController : ApiController
+    {
+        //La URL tendra que tener la siguiente sintaxis
+        ///time?value=16:30
+        //Aceptara valores de hora como por ejemplo
+        // 16:30
+        // 16:30:00
+        [HttpGet]
+        public JObject formateoHora(string value)
+        {
+            try
+            {
+                //Primero se parsea el valor obtenido de la url a la hora universal
+                string nueva_hora = DateTimeOffset.Parse(value).ToUniversalTime().ToString(); 
+                //Se crea un JOject (Json)
+                dynamic jsonTime = new JObject();
+                jsonTime.code = "00";
+                jsonTime.description = "OK";
+                //Se le agrega la hora parseada al Json
+                jsonTime.data = nueva_hora;
+                return jsonTime;
+            }
+            catch //Si ocurre cualquier error se manejara con el estado 500
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrio un error inesperado (Asegurese que la sintaxis de la hora es correcta: XX:XX o XX:XX:XX)"));
+            }
+        }
+    }
+}
---
+++ ServicioRest/Controllers/wordController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace ServicioRest.Controllers
+{
+    public class wordController : ApiController
+    {
+        //El Json debe seguir la siguiente sintaxis
+        //{ "data" : "hola" }
+        [HttpPost]
+        public JObject JsonPost(JObject palabraJson)
+        {
+            int max_caract = 0; //Si max caracter cambia a 1, es porque hay problemas con el largo del string
+            try
+            {
+                //Se valida que sea un string de 4 caracteres
+                if (palabraJson["data"].ToString().Length == 4)
+                {
+                    string data = palabraJson["data"].ToString();
+                    dynamic jsonWord = new JObject();
+
+                    jsonWord.code = "00";
+                    jsonWord.description = "OK";
+                    //Se pasan los caracteres del string a mayuscula
+                    jsonWord.data = data.ToUpper();
+
+                    return jsonWord;
+                }
+                else {//Si no es de largo 4, se cambia el valor de max_caract para poder enviar un error 400
+                    max_caract = 1;
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, "C"));
+                }
+            }
+            catch
+            {
+                if (max_caract == 1)//Se entrega el error 400 con el mensaje explicando lo que podría generarlo
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Cantidad de caracteres no soportados (Maximo de caracteres = 4)"));
+                //Pudiendo ser cualquier otro error lo manejamos con error 500
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Ocurrio un error inesperado (Revise sintaxis del json)"));
+            }
+        }
+    }
+}
